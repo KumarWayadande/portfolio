77f2da585bde04ae8ec2824b365c3a33fd4c1587
Kumar Wayadande <kumarwaidande7219@gmail.com>
2024-06-21 23:14:18 +0530

    Both mobile and desktop navbar contents separated to a separate file

--- src/components/mobile-navbar/Navbar.jsx
+++ src/components/mobile-navbar/Navbar.jsx
@@ -1,20 +1,19 @@
 import { createPortal } from "react-dom";
 import NavItemContainer from "./NavItemContainer";
 import NavItem from "./NavItem";
-import LandingPage from "../utility components/LandingPage";
-import ArticleContainer from "../article-components/ArticleContainer";
-import ProjectContainer from "../project-components/ProjectContainer";
-import Speaking from "../speaking-components/Speaking";
-import Uses from "../tools-components/ToolContainer";
 import { NavbarContext } from "../../store/NavbarContext";
 import { useContext } from "react";
+import { NAV_ITEM_INFO } from "../../contents/Nav-item-info";
 
 export default function Navbar() {
-  const {handleModalClick} = useContext(NavbarContext);
+  const { handleModalClick } = useContext(NavbarContext);
   // handleComponentClick
   return createPortal(
     // <nav className="md:hidden bg-[#050505e1] backdrop-blur-sm w-[100%]  z-10 absolute">
-    <nav onClick={handleModalClick} className="md:hidden bg-zinc-800/40 fixed backdrop-blur-sm dark:bg-black/80 opacity-100 w-[100%] z-40 inset-0">
+    <nav
+      onClick={handleModalClick}
+      className="md:hidden bg-zinc-800/40 fixed backdrop-blur-sm dark:bg-black/80 opacity-100 w-[100%] z-40 inset-0"
+    >
       <div className="inner-navbar-container dark:bg-zinc-900 bg-white w-[95%] mx-auto my-10 rounded-3xl flex flex-col">
         <div className="navigator flex items-center flex-row justify-between px-10 py-5">
           <div>
@@ -27,40 +26,24 @@ export default function Navbar() {
               className="text-2xl cursor-pointer dark:text-stone-300"
               onClick={(event) => {
                 event.stopPropagation();
-                handleModalClick();}}
+                handleModalClick();
+              }}
             >
               x
             </span>
           </div>
         </div>
         <NavItemContainer>
-          <>
-            <NavItem
-              content="About"
-              isBottomBorder
-              component={<LandingPage />}
-            />
-            <NavItem
-              content="Articles"
-              isBottomBorder
-              component={<ArticleContainer />}
-            />
-            <NavItem
-              content="Projects"
-              isBottomBorder
-              component={<ProjectContainer />}
-            />
-            <NavItem
-              content="Speaking"
-              isBottomBorder
-              component={<Speaking />}
-            />
-            <NavItem
-              content="Uses"
-              isBottomBorder={false}
-              component={<Uses />}
-            />
-          </>
+          {NAV_ITEM_INFO.map((item) => {
+            return (
+              <NavItem
+                key={item.id}
+                content={item.content}
+                isBottomBorder={item.isBottomBorder}
+                component={item.component}
+              />
+            );
+          })}
         </NavItemContainer>
       </div>
     </nav>,
