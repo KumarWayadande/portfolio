875d8b401cef66854959f1f559d27324d67555bc
Kumar Wayadande <kumarwaidande7219@gmail.com>
2024-07-15 10:05:22 +0530

    Adding server side code

---
+++ backend/routes/article.js
@@ -0,0 +1,20 @@
+import express from "express";
+const router = express.Router();
+
+
+
+
+
+// To get all the articles
+router.get("/", () => {
+
+    
+
+});
+
+// To get a specific article
+router.get("/:id", () => {});
+// To add a article
+router.post("/", () => {});
+// To update a specific article
+router.put("/:id", () => {});
