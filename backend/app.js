37fe6781689ab4a4a967b5cb2ee5c00793d550ab
Kumar Wayadande <kumarwaidande7219@gmail.com>
2024-07-22 16:53:43 +0530

    .env tried to add

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 import bodyParser from "body-parser";
 import express from "express";
 import articleRoute from "./routes/article.js";
+
 const app = express();
 
 app.use(bodyParser.json());
@@ -11,9 +12,7 @@ app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
   next();
 });
-
 app.use("/articles", articleRoute);
-
 app.use((error, req, res) => {
   const message = error.message || "Something went wrong!!!";
   const status = error.status || 500;
