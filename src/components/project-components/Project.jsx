32997034dccf0a70a05d45feb9b2979ecf615a70
Kumar Wayadande <kumarwaidande7219@gmail.com>
2024-08-03 15:14:52 +0530

    Responsiveness improved

--- src/components/project-components/Project.jsx
+++ src/components/project-components/Project.jsx
@@ -1,7 +1,7 @@
 import ProjectLink from './ProjectLink';
 const Project = (prop) => {
   return (
-    <div className="box1 group flex flex-col pl-3 pr-20 py-2 rounded-3xl hover:bg-gray-50 dark:hover:bg-[#252529] cursor-pointer">
+    <div className="box1 hover:duration-200 group flex flex-col pl-6 pr-16 py-2 rounded-3xl hover:bg-gray-50 dark:hover:bg-[#252529] cursor-pointer">
       <span className="rounded-3x">
         <img
           src={prop.projectLogo}
@@ -10,7 +10,7 @@ const Project = (prop) => {
         />
       </span>
       <h1 className="font-semibold my-2 dark:text-gray-200">{prop.projectHeader}</h1>
-      <p className="text-stone-500 text-sm my-2 dark:text-gray-500">
+      <p className="text-stone-500 text-sm my-2 dark:text-gray-500 line-clamp-3">
         {prop.projectDescription}
       </p>
       <ProjectLink buttonText={prop.buttonText} projectLink={prop.projectLink} />
