d5f966009f5ef20ed26b7e86aa746474b9529aae
Kumar Wayadande <kumarwaidande7219@gmail.com>
2024-08-03 19:55:53 +0530

    Adding shadcn

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,15 @@
+// import  flowbite  from "flowbite/plugin";
 /** @type {import('tailwindcss').Config} */
+
 export default {
-  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    // "node_modules/flowbite-react/lib/esm/**/*.js",
+  ],
   darkMode: "class",
   theme: {
     extend: {},
   },
+  // plugins: [flowbite],
   plugins: [],
-}
-
+};
