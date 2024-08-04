74283ce29e37724a74cd48d9d6d2c5a36883eaff
Kumar Wayadande <kumarwaidande7219@gmail.com>
2024-08-04 23:47:45 +0530

    React Quill implemented

--- src/components/article-components/single-article/SubHeading.jsx
+++ src/components/article-components/single-article/SubHeading.jsx
@@ -1,5 +1,5 @@
 export default function SubHeading(prop){
-    return <h1 className="font-normal md:text-3xl mt-10 mb-3 text-2xl dark:text-gray-100">
+    return <h1 className="font-medium md:text-3xl mt-10 mb-3 text-2xl dark:text-gray-100">
         {prop.children}
     </h1>
 }
--- src/components/editor/Main.jsx
+++ src/components/editor/Main.jsx
@@ -2,16 +2,14 @@ import { useState } from "react";
 import ReactQuill from "react-quill";
 import "react-quill/dist/quill.snow.css";
 import Parser from "html-react-parser";
-// import classes from "./styles.module.css";
+import classes from "./styles.module.css";
 import Contents from "../article-components/single-article/Contents";
 import Heading from "../article-components/single-article/Heading";
 import SubHeading from "../article-components/single-article/SubHeading";
 import CodeEditor from "../article-components/single-article/CodeEditor";
 import ExtLink from "../article-components/single-article/ExtLink";
 import Image from "../article-components/single-article/Image";
-import Date from "../article-components/single-article/Date";
 import banner from "../../assets/aero_function_banner-3.png";
-import ArticleBackButton from "../article-components/single-article/ArticleBackBtn";
 const modules = {
   toolbar: [
     [{ header: [1, 2, false] }],
@@ -35,21 +33,20 @@ const formats = [
 
 export default function Write() {
   const [value, setValue] = useState("");
+  const [title, setTitle] = useState("Demo Title will look like this");
   const parsedValues = Parser(value);
-
-  // console.log(parsedValues);
-  // console.log(parsedValues);
-  // console.log((parsedValues && parsedValues.length > 0) ? "Array" : "Object");
-  // console.log(parsedValues.length);
-
   let contents = "";
+
+  const handleSubmitDraft = () => {
+
+  }
 
   if (parsedValues && !parsedValues.length) {
     contents = parsedValues;
   }
 
   if (parsedValues && parsedValues.length > 0) {
-    contents = parsedValues.map((tag) => {
+    contents = [parsedValues.map((tag) => {
       switch (tag.type) {
         case "p":
           return <Contents key={tag.key}>{tag.props.children}</Contents>;
@@ -62,16 +59,26 @@ export default function Write() {
         default:
           return tag.props.children;
       }
-    });
+    })];
+  }
+
+
+  console.log(contents);
+  
+
+  const handleChangeTitle = (e) => {
+    setTitle(e.target.value);
   }
 
   return (
-    <div className="editor-preview-container flex flex-row">
-      <div className="editor-container">
-        <input type="text" placeholder="Title" name="title" id="title" />
+    <div className="editor-preview-container flex flex-col h-full md:h-[36rem] md:flex-row">
+      <div className="editor-container w-[100%] md:w-[500px] mr-[20px] basis-1/2">
+        <input type="text" onChange={handleChangeTitle} className="w-[100%] border my-3 py-1 px-2 border-gray-300" placeholder="Title of the Article" name="title" id="title" />
+        <label htmlFor="bannerImg" className="underline py-5">Upload an banner image</label>
+        <input type="file" name="bannerImg" className="hidden" id="bannerImg" />
         <div className="editor-container">
           <ReactQuill
-            className="editor"
+            className={classes.quillEditor}
             theme="snow"
             value={value}
             onChange={setValue}
@@ -80,216 +87,15 @@ export default function Write() {
           />
         </div>
       </div>
-      <div>
-        <div className="preview-container">
-          <div className="single-article-container mx-auto mt-15 md:mt-20 flex flex-col md:flex-row">
-            {/* <div className="back-aero-container md:basis-1/6 flex-col md:flex-row">
-              <div className="flex">
-                <ArticleBackButton />
-              </div>
-            </div> */}
-            <div className="article-contents-container max-w-2xl basis-10/12">
-              <Date />
-              <Heading extraClasses="mb-15">
-                Understanding Arrow Functions in JavaScript
-              </Heading>
-              <Contents>
-                The{" "}
-                <ExtLink link="http://www.ecma-international.org/ecma-262/6.0/">
-                  2015 edition of the ECMAScript specification (ES6)
-                </ExtLink>{" "}
-                added arrow function expressions to the{" "}
-                <ExtLink link="https://www.digitalocean.com/community/tutorial_series/how-to-code-in-javascript">
-                  Javascript
-                </ExtLink>{" "}
-                language. Arrow functions are a new way to write anonymous
-                function expressions, and are similar to
-                <ExtLink> lambda functions </ExtLink>in some other programming
-                languages, such as <ExtLink>Python.</ExtLink>
-              </Contents>
+        <div className="preview-container basis-1/2 overflow-y-scroll ">
+          <div className="single-article-container overflow-hidden">
+            <div className="article-contents-container">
+              <Heading>{title || "Put Demo title of your article"}</Heading>
               <Image src={banner} className="rounded-xl my-10" />
-              <Contents>
-                Arrow functions differ from traditional functions in a number of
-                ways, including the way their scope is determined and how their
-                syntax is expressed. Because of this, arrow functions are
-                particularly useful when passing a function as a parameter to a
-                higher-order function, such as when you are looping over an{" "}
-                <ExtLink>array</ExtLink> with{" "}
-                <ExtLink>built-in iterator methods</ExtLink>. Their syntactic
-                abbreviation can also allow you to improve the readability of
-                your code.
-              </Contents>
-              <Contents>
-                In this article, you will review function declarations and
-                expressions, learn about the differences between traditional
-                function expressions and arrow function expressions, learn about
-                lexical scope as it pertains to arrow functions, and explore
-                some of the syntactic shorthand permitted with arrow functions.
-              </Contents>
-              <Heading>Defining Functions</Heading>
-              <hr className="border-slate-200 dark:border-[#35353b]" />
-              <Contents>
-                Before delving into the specifics of arrow function expressions,
-                this tutorial will briefly review traditional JavaScript
-                functions in order to better show the unique aspects of arrow
-                functions later on.
-              </Contents>
-              <Contents>
-                The <ExtLink>How To Define Functions in JavaScript</ExtLink>{" "}
-                tutorial earlier in this series introduced the concept of
-                function declarations and function expressions. A function
-                declaration is a named function written with the{" "}
-                <var>function</var> keyword. Function declarations load into the
-                execution context before any code runs. This is known as
-                hoisting, meaning you can use the function before you declare
-                it.
-              </Contents>
-              <Contents>
-                Here is an example of a sum function that returns the sum of two
-                parameters:
-              </Contents>
-              <CodeEditor>
-                {`function sum(a, b){
-  return a + b;
-}`}
-              </CodeEditor>
-              <Contents>
-                You can execute the<var> sum</var> function before declaring the
-                function due to hoisting:
-              </Contents>
-              <CodeEditor>
-                {`sum(a, b);
-function sum(a, b){
-   return a + b;
-}`}
-              </CodeEditor>
-              <Contents>
-                Running this code would give the following output:
-              </Contents>
-              <CodeEditor isOutput>3</CodeEditor>
-              <Contents>
-                You can find the name of the function by logging the function
-                itself:
-              </Contents>
-              <CodeEditor>{`console.log(sum);`}</CodeEditor>
-              <Contents>
-                This will return the function, along with its name:
-              </Contents>
-              <CodeEditor isOutput>{`ƒ sum(a, b) {
-    return a + b
-}`}</CodeEditor>
-              <Contents>
-                A function expression is a function that is not pre-loaded into
-                the execution context, and only runs when the code encounters
-                it. Function expressions are usually assigned to a variable, and
-                can be anonymous, meaning the function has no name.
-              </Contents>
-              <Contents>
-                In this example, write the same <strong>sum</strong> function as
-                an anonymous function expression:
-              </Contents>
-              <CodeEditor>{`const sum = function (a, b) {
-    return a + b
-}`}</CodeEditor>
-              <Contents>
-                You`&apos;`ve now assigned the anonymous function to the{" "}
-                <var>sum</var> constant. Attempting to execute the function
-                before it is declared will result in an error:
-              </Contents>
-              <CodeEditor>
-                {`sum(1, 2)
-
-const sum = function (a, b) {
-    return a + b
-}`}
-              </CodeEditor>
-              <Contents>Running this will give:</Contents>
-              <CodeEditor
-                isOutput
-              >{`Uncaught ReferenceError: Cannot access 'sum' before initialization`}</CodeEditor>
-              <Contents>
-                Also, note that the function does not have a named identifier.
-                To illustrate this, write the same anonymous function assigned
-                to <var>sum</var>, then log <var>sum</var> to the console:
-              </Contents>
-              <CodeEditor>
-                {`const sum = function (a, b) {
-  return a + b
-}
-
-console.log(sum)`}
-              </CodeEditor>
-              <Contents>This will show you the following:</Contents>
-              <CodeEditor isOutput>{`ƒ (a, b) {
-  return a + b
-}`}</CodeEditor>
-              <Contents>
-                The value of sum is an anonymous function, not a named function.
-              </Contents>
-              <Contents>
-                You can name <strong>function</strong> expressions written with
-                the function keyword, but this is not popular in practice. One
-                reason you might want to name a function expression is to make
-                error stack traces easier to debug.
-              </Contents>
-              <Contents>
-                Consider the following function, which uses an{" "}
-                <ExtLink>if statement</ExtLink> to throw an error if the
-                function parameters are missing:
-              </Contents>
-              <CodeEditor>
-                {`const sum = function namedSumFunction(a, b) {
-    if (!a || !b) 
-      throw new Error('Parameters are required.')
-    
-    return a + b
-}
-
-sum();`}
-              </CodeEditor>
-              <Contents>
-                The highlighted section gives the function a name, and then the
-                function uses the <strong>or ||</strong> operator to throw an
-                error <ExtLink>object</ExtLink> if either of the parameters is
-                missing.
-              </Contents>
-
-              <Contents>
-                Running this code will give you the following:
-              </Contents>
-              <CodeEditor isOutput>{`Uncaught Error: Parameters are required.
-  at namedSumFunction (<anonymous>:3:23)
-  at <anonymous>:1:1`}</CodeEditor>
-              <Contents>
-                In this case, naming the function gives you a quick idea of
-                where the error is.
-              </Contents>
-              <Contents>
-                An <ExtLink>arrow function expression</ExtLink> is an anonymous
-                function expression written with the &quot;fat arrow&quot;
-                syntax (=&gt;).
-              </Contents>
-              <Contents>
-                Rewrite the <strong>sum</strong> function with arrow function
-                syntax:
-              </Contents>
-              <CodeEditor>
-                {`const sum = (a, b) => {
-   return a + b
-}`}
-              </CodeEditor>
-              <Contents>
-                Like traditional function expressions, arrow functions are not
-                hoisted, and so you cannnot call them before you declare them.
-                They are also always anonymous—there is no way to name an arrow
-                function. In the next section, you will explore more of the
-                syntactical and practical differences between arrow functions
-                and traditional functions.
-              </Contents>
+              {contents}
             </div>
           </div>
         </div>
       </div>
-    </div>
   );
 }
