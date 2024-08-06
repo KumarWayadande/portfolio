8281a5f6dba0116db7091ffd186860d65040daa1
Kumar Wayadande <kumarwaidande7219@gmail.com>
2024-08-06 20:29:33 +0530

    Navbar item improved

--- src/components/desktop-navbar/NavbarItem.jsx
+++ src/components/desktop-navbar/NavbarItem.jsx
@@ -1,14 +1,25 @@
 import { NavLink } from "react-router-dom";
 
+
+// Navbar item - It displays navbar items (about, articles, projects...)
 const NavbarItem = (prop) => {
+  // Classes when link is inactive
   let initialClass =
     "dark:text-gray-50 dark:hover:text-[#1fbcaa] nav-item font-semibold text-sm cursor-pointer hover:text-[#1fbcaa]";
 
+  // Classes when link is active
   const newClass =
     "dark:hover:text-[#1fbcaa] nav-item font-semibold text-sm cursor-pointer hover:text-[#1fbcaa] text-[#1fbcaa] dark:text-[#1fbcaa]";
 
   return (
     <>
+      {/* 
+
+      React-router-dom <Navlink /> component 
+      It displays navbar items (about, articles, projects...)
+      When clicked then renders the desired page
+
+    */}
       <NavLink
         to={prop.path}
         className={({ isActive }) => (isActive ? newClass : initialClass)}
