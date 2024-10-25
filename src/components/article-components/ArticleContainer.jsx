28c4d8cc6c0619161577b0f525cf4a1fe890598f
Kumar Wayadande <kumarwaidande7219@gmail.com>
2024-10-25 08:33:58 +0530

    Articles Component improved

--- src/components/article-components/ArticleContainer.jsx
+++ src/components/article-components/ArticleContainer.jsx
@@ -5,19 +5,20 @@ import SubHeading from "../utility components/SubHeading.jsx";
 import axios from "axios";
 import { useLoaderData } from "react-router-dom";
 
-
-
 // Component that acts as a wrapper for all articles
 // This gives view of all articles in abstract way
 const ArticleContainer = () => {
-  
   // Load all articles from loader function of this component itself
-  const { data } = useLoaderData();   // data.articles -> ARTICLES
+  const { data } = useLoaderData(); // data.articles -> ARTICLES
   const { articles: ARTICLES } = data;
+
+  console.log(ARTICLES);
+  
+  const lengthOfArticles = ARTICLES.length;
 
   return (
     <div className="article-container my-20">
-    {/* Main heading of page */}
+      {/* Main heading of page */}
       <HeadingContainer>
         <Heading
           heading="Writing on software design, company building, and the aerospace
@@ -29,13 +30,23 @@ const ArticleContainer = () => {
         />
       </HeadingContainer>
 
-      <div className="article-card-container flex flex-col border-l-[1px] sm:w-[100%] dark:border-gray-700 border-b-gray-50 md:w-[80%] sm:pl-5">
-        {/* All articles are listed and displayed in abstract way */}
-        {ARTICLES.map((article, index) => {
-          return <Article {...article} key={index} />;    // Article wrapper
-        })}
-        {/* End of all articles */}
-      </div>
+      {lengthOfArticles > 0 && (
+        <div className="article-card-container flex flex-col border-l-[1px] sm:w-[100%] dark:border-gray-700 border-b-gray-50 md:w-[80%] sm:pl-5">
+          {/* All articles are listed and displayed in abstract way */}
+          {ARTICLES.map((article, index) => {
+            {
+              /* return <Article {...article} key={index} />;    // Article wrapper */
+            }
+            return (
+              <Article {...ARTICLES[lengthOfArticles - 1 - index]} key={index} />
+            ); // Article wrapper
+          })}
+          {/* End of all articles */}
+        </div>
+      )}
+      {
+        lengthOfArticles == 0 && <p>No Articles Found!!!</p>
+      }
     </div>
   );
 };
